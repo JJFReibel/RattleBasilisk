c2e24405f10f4b4afaa8e7c39a436bd1d48abaaf
Jean-Jacques François Reibel <9219877+JJFReibel@users.noreply.github.com>
2021-09-04 13:19:49 -0700

    RattleBasilisk

---
+++ basilisk.py
@@ -0,0 +1,14 @@
+# April Fools Script 2
+# By Jean-Jacques F. Reibel
+# I will not be held responsible for:
+# any shenanigans
+import os
+
+os.system("printf '\e[0;31;1;1m\n'")
+os.system("printf '\e[1;32;1;1m                /\/\/\/\/\/\/\/\/\/\/\/\\\n'")
+os.system("printf '\e[0;31;1;5m          ~'")
+os.system("printf '\e[0;31;1;1m>'")
+os.system("printf '\e[0;33;1;1m°'")
+os.system("printf '\e[1;32;1;1m)～～～～～～～～～～～～～～###\n'")
+os.system("printf '\e[0;31;1;1m\n'")
+
